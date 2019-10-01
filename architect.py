487dd1bee178205b7bc7fa2e1e41ff1b23d31d13
IlyaTrofimov <i-tr@yandex.ru>
2019-10-01 14:09:19 +0300

    Add HOAG.

--- architect.py
+++ architect.py
@@ -1,7 +1,8 @@
 """ Architect controls architecture of cell by computing gradients of alphas """
 import copy
 import torch
-
+from scipy.sparse.linalg import cg, LinearOperator
+import numpy as np
 
 class Architect():
     """ Compute gradients of alphas """
@@ -56,45 +57,117 @@ class Architect():
             xi: learning rate for virtual gradient step (same as net lr)
             w_optim: weights optimizer - for virtual step
         """
-        # do virtual step (calc w`)
-        self.virtual_step(trn_X, trn_y, xi, w_optim)
-
-        # calc unrolled loss
-        loss = self.v_net.loss(val_X, val_y) # L_val(w`)
-
         # compute gradient
-        v_alphas = tuple(self.v_net.alphas())
-        v_weights = tuple(self.v_net.weights())
-        v_grads = torch.autograd.grad(loss, v_alphas + v_weights)
-        dalpha = v_grads[:len(v_alphas)]
-        dw = v_grads[len(v_alphas):]
 
+        def ddot(a, b):
+           return sum(u.flatten() @ v.flatten() for u, v in zip(a, b))
+
+        if self.hessian_vector_type in [0, 1, 2]:
+            # do virtual step (calc w`)
+            self.virtual_step(trn_X, trn_y, xi, w_optim)
+
+            # calc unrolled loss
+            val_loss = self.v_net.loss(val_X, val_y) # L_val(w`)
+
+            v_alphas = tuple(self.v_net.alphas())
+            v_weights = tuple(self.v_net.weights())
+            v_grads = torch.autograd.grad(val_loss, v_alphas + v_weights)
+            dalpha = v_grads[:len(v_alphas)] # d_a L_val(w')
+            dw = v_grads[len(v_alphas):]     # d_w L_val(w')
+
+        #
+        # DARTS
+        #
         if self.hessian_vector_type == 1:
-            hessian_vector = self.compute_hessian(dw, trn_X, trn_y)
+            hessian_vector = self.compute_hessian_vector(dw, trn_X, trn_y)
+
+        #
+        # Exact hessian*grad
+        #
         elif self.hessian_vector_type == 2:
-
-            def ddot(a, b):
-                return sum(u.flatten() @ v.flatten() for u, v in zip(a, b))
-
-            # TROFIM
             tr_loss = self.net.loss(trn_X, trn_y) # L_train(w)
-            grad_w = torch.autograd.grad(tr_loss, self.net.weights(), create_graph = True)
-            grad_vector = ddot(grad_w, dw)
+            grad_L_train_w = torch.autograd.grad(tr_loss, self.net.weights(), create_graph = True)
+            grad_vector = ddot(grad_L_train_w, dw)
 
             hessian_vector = torch.autograd.grad(grad_vector, self.net.alphas(), retain_graph = False)
+
+        #
+        # HOAG (hessian * inverse_hessian * grad)
+        #
+        elif self.hessian_vector_type == 3:
+            # calc unrolled loss
+            val_loss = self.net.loss(val_X, val_y) # L_val(w)
+
+            # compute gradient
+            v_alphas = tuple(self.net.alphas())
+            v_weights = tuple(self.net.weights())
+            v_grads = torch.autograd.grad(val_loss, v_alphas + v_weights)
+            dalpha = v_grads[:len(v_alphas)] # d_a L_val
+            dw = v_grads[len(v_alphas):]     # d_w L_val
+
+            tr_loss = self.net.loss(trn_X, trn_y) # L_train(w)
+            grad_L_train_w = torch.autograd.grad(tr_loss, self.net.weights(), create_graph = True)
+
+            device = tr_loss.device
+
+            N = sum([x.numel() for x in dw])
+
+            def to_flat(t):
+                return torch.cat([x.flatten().cpu() for x in t], 0)
+
+            def calc_huge_hessian_vector(z):
+
+                grad_vector = 0
+                idx = 0
+                z_pt = torch.Tensor(z).to(device)
+                items = []
+
+                tr_loss = self.net.loss(trn_X, trn_y) # L_train(w)
+                grad_L_train_w = torch.autograd.grad(tr_loss, self.net.weights(), create_graph = True) # d_w L_train(w)
+
+                for elem in grad_L_train_w:
+                    grad_vector += elem.flatten() @ z_pt[idx : idx + elem.numel()]
+                    idx += elem.numel()
+
+                huge_hessian_vector = torch.autograd.grad(grad_vector, self.net.weights(), retain_graph = False)
+
+                return to_flat(huge_hessian_vector)
+
+            LinOp = LinearOperator((N, N), matvec = calc_huge_hessian_vector, dtype = np.dtype('float64'))
+
+            inv_hessian_vector = cg(LinOp, to_flat(dw), tol = 1e-3, maxiter = 5)[0]
+            inv_hessian_vector = torch.Tensor(inv_hessian_vector).to(device)
+
+            #
+            #  Restore tensor dimensions
+            #
+            inv_hessian_vector_torch = []
+            idx = 0
+
+            for elem in dw:
+                n = elem.numel()
+                shape = elem.shape
+                inv_hessian_vector_torch.append(inv_hessian_vector[idx : idx + n].reshape(shape))
+                idx += n
+
+            hessian_vector = self.compute_hessian_vector(inv_hessian_vector_torch, trn_X, trn_y)
         else:
             hessian_vector = 0
 
         # update final gradient = dalpha - xi*hessian
         with torch.no_grad():
-            if self.hessian_vector_type > 0:
-                for alpha, da, h in zip(self.net.alphas(), dalpha, hessian_vector):
-                    alpha.grad = da - xi*h
-            else:
+            if self.hessian_vector_type == 0:
                 for alpha, da in zip(self.net.alphas(), dalpha):
                     alpha.grad = da
+            elif self.hessian_vector_type in [1, 2]:
+                for alpha, da, h in zip(self.net.alphas(), dalpha, hessian_vector):
+                    alpha.grad = da - xi*h
+            elif self.hessian_vector_type == 3:
+                for alpha, da, h in zip(self.net.alphas(), dalpha, hessian_vector):
+                    alpha.grad = da - h
 
-    def compute_hessian(self, dw, trn_X, trn_y):
+
+    def compute_hessian_vector(self, dw, trn_X, trn_y):
         """
         dw = dw` { L_val(w`, alpha) }
         w+ = w + eps * dw
